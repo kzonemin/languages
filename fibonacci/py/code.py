d1439748b6da904ccd4053abb6cd7c7255842912
kzonemin <160205607+kzonemin@users.noreply.github.com>
2024-11-28 10:07:23 +0530

    Update code.py

--- fibonacci/py/code.py
+++ fibonacci/py/code.py
@@ -1,14 +1,17 @@
 import sys
 
-def fibonacci(n):
-  if (n == 0):
-    return 0
-  if (n == 1):
-    return 1
-  return fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+    return memo[n]
 
 u = int(sys.argv[1])
 r = 0
 for i in range(u):
-  r += fibonacci(i)
+    r += fibonacci(i)
 print(r)
